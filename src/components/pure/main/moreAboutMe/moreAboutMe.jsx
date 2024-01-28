e10445303642bef248d8e1132f0e539972d0e400
Crisppu <cristobalyat69@hotmail.com>
2024-01-27 23:34:03 -0600

    tercera prueba modificando el background en el componente

--- src/components/pure/main/moreAboutMe/moreAboutMe.jsx
+++ src/components/pure/main/moreAboutMe/moreAboutMe.jsx
@@ -1,6 +1,9 @@
+import areaTrabajo from '../../../../../src/assets/img/areaTrabajo.webp';
 const MoreAboutMe = () => {
+   
     return (
-        <div className='main__moreAboutMe'>
+        <div className='main__moreAboutMe' style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${areaTrabajo})`,
+        backgroundRepeat:'no-repeat', backgroundSize:'cover',backgroundPosition:'center'}}>
             <span className='main__moreAboutMe__title'>Saber mas de mi </span>
             <button className='main__moreAboutMe__buttonCv'> ver cv </button>
         </div>
